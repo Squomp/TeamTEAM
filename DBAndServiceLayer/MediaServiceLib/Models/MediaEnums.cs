b76c6fba7871fcb1f5853f9153c64c368194cc04
Mitchell Hartweg <MHartweg@student.neumont.edu>
2017-11-10 18:47:28 -0700

    first commit

---
+++ DBAndServiceLayer/MediaServiceLib/Models/MediaEnums.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaServiceLib.Models
+{
+    public enum MediaType
+    {
+        Video, Image, Gif, Audio
+    }
+
+    public enum MediaCategory
+    {
+        Funny, News, Sports
+    }
+}
