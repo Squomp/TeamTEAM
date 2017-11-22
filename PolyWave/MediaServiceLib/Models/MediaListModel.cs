22960654d99f93f93043c8c881fb55df1318fe48
Mitchell Hartweg <mhartweg@student.neumont.edu>
2017-11-21 17:57:39 -0700

    readding project

---
+++ PolyWave/MediaServiceLib/Models/MediaListModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaServiceLib.Models
+{
+    public class MediaListModel
+    {
+        public List<MediaModel> MediaList { get; set; }
+
+        public MediaListModel()
+        {
+            MediaList = new List<MediaModel>();
+        }
+    }
+}
---
+++ PolyWave/MediaServiceLib/Models/MediaModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaServiceLib.Models
+{
+    public class MediaModel
+    {
+        public int Id { get; set; }
+        public string Author { get; set; }
+        public string Caption { get; set; }
+        public string Description { get; set; }
+        public string Path { get; set; }
+        public MediaType Type { get; set; }
+        public MediaCategory Category { get; set; }
+    }
+}
